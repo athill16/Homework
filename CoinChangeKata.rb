6c89c1a75116fcc8a4a1763b131ec053cc9523ed
Aaron Hill <aaronthill16@gmail.com>
2016-02-20 16:26:22 -0500

    Missed step, this step is done before the last cleaning of the code

--- CoinChangeKata.rb
+++ CoinChangeKata.rb
@@ -1,12 +1,29 @@
 def coin_change(amount)
-	coins = {"quarters" => 25, "dimes" => 10, "nickels" => 5, "pennies" => 1}
-	amount_of_coins = {"quarters" => 0, "dimes" => 0, "nickels" => 0, "pennies" => 0}
-	
-	coins.each do |coin, value|
-		while amount >= value
-			amount_of_coins[coin] = amount_of_coins[coin] + 1
-			amount = amount - value
-		end
+	coins = {"quarters" => 0, "dimes" => 0, "nickels" => 0, "pennies" => 0}
+	quarters = 25
+	dimes = 10
+	nickels = 5
+	pennies = 1
+
+	while amount >= quarters
+		coins["quarters"] = coins["quarters"] + 1
+		amount = amount - 25
 	end
-	amount_of_coins
+	
+	while amount >= dimes
+		coins["dimes"] = coins["dimes"] + 1
+		amount = amount - 10
+	end
+
+	while amount >= nickels
+		coins["nickels"] = coins["nickels"] + 1
+		amount = amount - 5
+	end
+
+	while amount >= pennies
+		coins["pennies"] = coins["pennies"] + 1
+		amount = amount - 1
+	end
+	coins
 end
+
