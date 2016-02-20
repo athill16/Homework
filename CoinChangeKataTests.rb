96b603023be0c9ab141ac52b0e314f551ce626ae
Aaron Hill <aaronthill16@gmail.com>
2016-02-20 15:41:19 -0500

    test that there's one of each coin

--- CoinChangeKataTests.rb
+++ CoinChangeKataTests.rb
@@ -43,6 +43,10 @@ class TestCoinChange < Minitest::Test
 		assert_equal({"quarters" => 1, "dimes" => 0, "nickels" => 0, "pennies" => 0}, coin_change(25))
 	end
 
+	def test_change_is_41
+		assert_equal({"quarters" => 1, "dimes" => 1, "nickels" => 1, "pennies" => 1}, coin_change(41))
+	end
+	
 end
 
 
