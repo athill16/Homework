a3e438a2226180c0c6be6533489e8eb8b8528258
Aaron Hill <aaronthill16@gmail.com>
2016-02-20 15:39:32 -0500

    made code cleaner

--- CoinChangeKata.rb
+++ CoinChangeKata.rb
@@ -1,19 +1,12 @@
 def coin_change(amount)
-	coins = {"quarters" => 0, "dimes" => 0, "nickels" => 0, "pennies" => 0}
-	if amount >= 25
-		coins["quarters"] = 1
-	elsif amount >= 15
-		coins["dimes"] = 1
-		coins["nickels"] = 1
-		coins["pennies"] = amount - 15
-	elsif amount >= 10
-		coins["dimes"] = 1
-		coins["pennies"] = amount - 10
-	elsif amount >= 5
-		coins["nickels"] = 1 
-		coins["pennies"] = amount - 5
-	else
-	coins = {"quarters" => 0, "dimes" => 0, "nickels" => 0, "pennies" => amount}
+	coins = {"quarters" => 25, "dimes" => 10, "nickels" => 5, "pennies" => 1}
+	amount_of_coins = {"quarters" => 0, "dimes" => 0, "nickels" => 0, "pennies" => 0}
+	
+	coins.each do |coin, value|
+		while amount >= value
+			amount_of_coins[coin] = amount_of_coins[coin] + 1
+			amount = amount - value
+		end
 	end
-	coins
+	amount_of_coins
 end
