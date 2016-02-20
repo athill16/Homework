cd7af7bb56d95cb422e48182f2bd719c06ffa5e5
Aaron Hill <aaronthill16@gmail.com>
2016-02-20 14:57:28 -0500

    coin change first test

--- CoinChangeKataTests.rb
+++ CoinChangeKataTests.rb
@@ -4,6 +4,7 @@ require_relative "CoinChangeKata.rb"
 class TestCoinChange < Minitest::Test
 
 	def test_change_is_0
-		
+		assert_equal({"quarters" => 0, "dimes" => 0, "nickels" => 0, "pennies" => 0}, coin_change(0))
+	end	
 
 end
